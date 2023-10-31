e83a4e74dec486e9e244fab2ea4fe5e7e0cc9259
dera ragdabio <dera@ragdabio.com>
2023-10-31 10:56:38 +0700

    fix: table user & add modal

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,16 +1,32 @@
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
 import { Link } from "react-router-dom";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Props = {
   columns: GridColDef[];
-  row: object[];
+  rows: object[];
   slug: string;
 };
 
 const DataTable = (props: Props) => {
+  // TEST THE API
+
+  // const queryClient = useQueryClient();
+  // // const mutation = useMutation({
+  // //   mutationFn: (id: number) => {
+  // //     return fetch(`http://localhost:8800/api/${props.slug}/${id}`, {
+  // //       method: "delete",
+  // //     });
+  // //   },
+  // //   onSuccess: ()=>{
+  // //     queryClient.invalidateQueries([`all${props.slug}`]);
+  // //   }
+  // // });
+
   const handleDelete = (id: number) => {
-    console.log(id + "has been deleted");
+    //delete the item
+    // mutation.mutate(id)
   };
 
   const actionColumn: GridColDef = {
@@ -35,7 +51,7 @@ const DataTable = (props: Props) => {
     <div className="dataTable">
       <DataGrid
         className="dataGrid"
-        rows={props.row}
+        rows={props.rows}
         columns={[...props.columns, actionColumn]}
         initialState={{
           pagination: {
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,10 @@
 import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
-import "./users.scss";
-import { userRows } from "../../data";
+import "./Users.scss";
 import { useState } from "react";
 import Add from "../../components/add/Add";
+import { userRows } from "../../data";
+// import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
@@ -53,20 +54,36 @@ const columns: GridColDef[] = [
   },
 ];
 
-const User = () => {
-  const [openModalAdd, setOpenModalAdd] = useState(false);
+const Users = () => {
+  const [open, setOpen] = useState(false);
+
+  // TEST THE API
+
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ["allusers"],
+  //   queryFn: () =>
+  //     fetch("http://localhost:8800/api/users").then(
+  //       (res) => res.json()
+  //     ),
+  // });
+
   return (
-    <div className="user">
+    <div className="users">
       <div className="info">
-        <h1>User</h1>
-        <button onClick={() => setOpenModalAdd(true)}>Add New User</button>
-        <DataTable slug="users" columns={columns} row={userRows} />
-        {openModalAdd && (
-          <Add setOpen={setOpenModalAdd} slug={"user"} columns={columns} />
-        )}
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {/* TEST THE API */}
+
+      {/* {isLoading ? (
+        "Loading..."
+      ) : (
+        <DataTable slug="users" columns={columns} rows={data} />
+      )} */}
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
 
-export default User;
+export default Users;
